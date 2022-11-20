437d63f8db3f4904f98f3b06aa49bc16bd3430e4
UnityTheCoder <unitythecodergit@gmail.com>
2022-11-20 23:11:49 +0100

    Double Enc

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 import (
-  xor "github.com/UnityTheCoder/xor/enc"
+  xor "github.com/UnityTheCoder/xor/xor"
   "fmt"
 )
 
 func main() {
-  key := "QXA5F"
-  str := "SecretPassword"
+  key := 431
+  key2 := "FIHU33"
+  str := "SecretPassword DF"
 
 
-  encrypted := xor.encrypt(str)
+  encrypted := xor.DoubleEncrypt(str, key, key2)
   fmt.Println("Encrypted:", encrypted)
 
-  decrypted := xor.decrypt(encrypted)
+  decrypted := xor.DoubleDecrypt(encrypted, key, key2)
   fmt.Println("Decrypted:", decrypted)
 }
--- xor/xor.go
+++ xor/xor.go
@@ -1,18 +1,32 @@
 package xor
 
+import "fmt"
 
-func encrypt(input, key string) (output string) {
-    for i := 0; i < len(input); i++ {
-      output += string(input[i] ^ key[i % len(key)])
-    }
-    return output
+func Encrypt(input, key string) (output string) {
+    kL := len(key)
+	  for i := range input {
+		  output += string(input[i] ^ key[i%kL])
+	  }
+	  return output
 }
 
-func decrypt(input, key string) (output string) {
-    for i := 0; i < len(input); i++ {
-      output += string(input[i] ^ key[i % len(key)])
-    }
-    return output
+func DoubleEncrypt(input, key1, key2 string) (output string) {
+  first_layer := Encrypt(input, key1)
+  second_layer := Encrypt(first_layer, key2)
+  return second_layer
+}
+
+func DoubleDecrypt(input, key1, key2 string) (output string) {
+  second_layer := Decrypt(input, key2)
+  first_layer := Decrypt(second_layer, key1)
+  return first_layer
 }
 
 
+func Decrypt(input, key string) (output string) {
+    kL := len(key)
+	  for i := range input {
+		  output += string(input[i] ^ key[i%kL])
+	  }
+	  return output
+}
